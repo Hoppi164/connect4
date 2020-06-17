96766a3097fca07aa2c88034fb0f8a7acdd64a00
drew <hoppi164@gmail.com>
2020-06-17 11:47:44 +1000

    Created basic "Hello World" react site template

---
+++ index.js
@@ -0,0 +1,23 @@
+const e = React.createElement;
+
+class Game extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			redsTurn: true
+		};
+	}
+
+	render() {
+		return (
+			<div>
+				<h1> Insert Connect4 Table! </h1>
+				<h1> Insert Connect4 info! </h1>
+			</div>
+		);
+	}
+}
+
+const domContainer = document.querySelector('#root');
+ReactDOM.render(e(Game), domContainer);
+
