f41b1317e8f307b71dc39566bfb54071d75f90b4
drew <hoppi164@gmail.com>
2020-06-17 12:34:42 +1000

    added npm app files

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <div>
+      <h1> Insert Connect4 Table! </h1>
+      <h1> Insert Connect4 info! </h1>
+    </div>
+
+  );
+}
+
+export default App;
+
+
+
+// const e = React.createElement;
+
+// class Game extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       redsTurn: true
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1> Insert Connect4 Table! </h1>
+//         <h1> Insert Connect4 info! </h1>
+//       </div>
+//     );
+//   }
+// }
+
+// const domContainer = document.querySelector('#root');
+// ReactDOM.render(e(Game), domContainer);
+
