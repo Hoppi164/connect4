ea331adfa806ea419b3d852298de99e8a60db6d5
drew <Hoppi164@gmail.com>
2020-06-20 12:06:42 +1000

    Added Turn-Toggle react hook

--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+    const [redsTurn, setRedsTurn] = useState(true);
+    return (
 
-    <div>
-      <h1> Insert Connect4 Table! </h1>
-      <h1> Insert Connect4 info! </h1>
-    </div>
+        <div>
+			<h1> Insert Connect4 Table! </h1>
+			<h1> Insert Connect4 info! </h1>
+			<h1> {redsTurn ? "Red":"Yellow"} Turn! </h1>
+			<button onClick={()=> setRedsTurn(!redsTurn)}> End Turn </button>
+		</div>
 
-  );
+    );
 }
 
 export default App;
-
-
-
-// const e = React.createElement;
-
-// class Game extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       redsTurn: true
-//     };
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <h1> Insert Connect4 Table! </h1>
-//         <h1> Insert Connect4 info! </h1>
-//       </div>
-//     );
-//   }
-// }
-
-// const domContainer = document.querySelector('#root');
-// ReactDOM.render(e(Game), domContainer);
-
