2d166e91b787a07eb20984091f7b71c94b197842
LorenzoLim <renzo-lim@hotmail.com>
2020-06-25 09:15:48 +1000

    Removed unused logo and fixed indentation

--- src/App.js
+++ src/App.js
@@ -1,99 +1,98 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import _ from "lodash"; // Import the entire lodash library
 import { makeStyles } from "@material-ui/core/styles";
 import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
 import {
-	Button,
-	Table,
-	TableBody,
-	TableCell,
-	TableContainer,
-	TableHead,
-	TableRow,
-	Paper,
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper
 } from "@material-ui/core";
 
 const useStyles = makeStyles({
-	table: {
-		minWidth: 650,
-	},
+  table: {
+    minWidth: 650
+  }
 });
 
 function App() {
-	// Define Const vars
-	const classes = useStyles();
-	const numCols = 7;
-	const numRows = 6;
+  // Define Const vars
+  const classes = useStyles();
+  const numCols = 7;
+  const numRows = 6;
 
-	// Create row x col sized empty array
-	let initialGrid = Array.from({ length: numRows }, () =>
-		Array.from({ length: numCols }, () => 0)
-	);
+  // Create row x col sized empty array
+  let initialGrid = Array.from({ length: numRows }, () =>
+    Array.from({ length: numCols }, () => 0)
+  );
 
-	// Define state Vars
-	const [redsTurn, setRedsTurn] = useState(true);
-	const [grid, setGrid] = useState(initialGrid);
+  // Define state Vars
+  const [redsTurn, setRedsTurn] = useState(true);
+  const [grid, setGrid] = useState(initialGrid);
 
-	const dropCoin = (colnum) => {
-		const newGrid = _.cloneDeep(grid); //create a deep clone of the grid
+  const dropCoin = colnum => {
+    const newGrid = _.cloneDeep(grid); //create a deep clone of the grid
 
-		// loop from bottom to top
-		for (var rowNum = newGrid.length - 1; rowNum >= 0; rowNum--) {
-			// if cell is null
-			if (!newGrid[rowNum][colnum]) {
-				let color = redsTurn ? "Red" : "Yellow"; //determine who's turn it is
-				newGrid[rowNum][colnum] = color; // add coin to cell
-				setRedsTurn(!redsTurn); // Toggle turn
-				break;
-			}
-		}
+    // loop from bottom to top
+    for (var rowNum = newGrid.length - 1; rowNum >= 0; rowNum--) {
+      // if cell is null
+      if (!newGrid[rowNum][colnum]) {
+        let color = redsTurn ? "Red" : "Yellow"; //determine who's turn it is
+        newGrid[rowNum][colnum] = color; // add coin to cell
+        setRedsTurn(!redsTurn); // Toggle turn
+        break;
+      }
+    }
 
-		// Update the grid
-		setGrid(newGrid);
-	};
+    // Update the grid
+    setGrid(newGrid);
+  };
 
-	// Return App HTML
-	return (
-		<div>
-			<h1> Insert Connect4 Table! </h1>
-			<h1> Insert Connect4 info! </h1>
-			<h1> {redsTurn ? "Red" : "Yellow"} Turn! </h1>
-			<button onClick={() => setRedsTurn(!redsTurn)}> End Turn </button>
+  // Return App HTML
+  return (
+    <div>
+      <h1> Insert Connect4 Table! </h1>
+      <h1> Insert Connect4 info! </h1>
+      <h1> {redsTurn ? "Red" : "Yellow"} Turn! </h1>
+      <button onClick={() => setRedsTurn(!redsTurn)}> End Turn </button>
 
-			<TableContainer component={Paper}>
-				<Table className={classes.table}>
-					<TableHead>
-						<TableRow>
-							{grid[0].map((col, index) => (
-								<TableCell align="center" key={index}>
-									<Button
-										variant="contained"
-										color="primary"
-										onClick={() => dropCoin(index)}
-									>
-										<ArrowDownwardIcon />
-									</Button>
-								</TableCell>
-							))}
-						</TableRow>
-					</TableHead>
-					<TableBody>
-						{grid.map((row, index) => (
-							<TableRow key={index}>
-								{row.map((cell, index) => (
-									<TableCell align="center" key={index}>
-										{cell}
-									</TableCell>
-								))}
-							</TableRow>
-						))}
-					</TableBody>
-				</Table>
-			</TableContainer>
-		</div>
-	);
+      <TableContainer component={Paper}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              {grid[0].map((col, index) => (
+                <TableCell align="center" key={index}>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => dropCoin(index)}
+                  >
+                    <ArrowDownwardIcon />
+                  </Button>
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {grid.map((row, index) => (
+              <TableRow key={index}>
+                {row.map((cell, index) => (
+                  <TableCell align="center" key={index}>
+                    {cell}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 }
 
 export default App;
