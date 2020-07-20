edca120cf7214a33140a87dfd6d5d398f2196f10
LorenzoLim <renzo-lim@hotmail.com>
2020-07-20 15:50:14 +1000

    Separated code from app.js into individual folders

--- react-ui/src/pages/ConnectFour.js
+++ react-ui/src/pages/ConnectFour.js
@@ -1,8 +1,25 @@
-import * as React from 'react'
+import React, { useEffect, useState } from "react";
+import gameEventHandlers from '../utils/gameEventHandlers';
+import GameMenu from "../components/GameMenu";
+import GameBoard from "../components/GameBoard";
+import { Container } from "@material-ui/core";
 
-class ConnectFour extends React.component {
-    const [redsTurn, setRedsTurn] = useState(true);
-    const [grid, setGrid] = useState(initialGrid);
+function ConnectFour() {
+    const [message, setMessage] = useState("Fetching Message");
+    useEffect(() => {
+        gameEventHandlers.createListeners(setMessage);
+    }, []);
+
+    // Return App HTML
+    return (
+        <div>
+            <GameMenu />
+            <Container>
+                <h1>{message}</h1>
+            </Container>
+            {/* TODO Passing message through to GameBoard */}
+            <GameBoard />
+        </div>
+    );
 }
-
 export default ConnectFour
