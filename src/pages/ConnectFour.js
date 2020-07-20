7dcbaf06540a89e1c3db78d600b4b6786167e2da
LorenzoLim <renzo-lim@hotmail.com>
2020-07-20 13:43:22 +1000

    Added Pages folder

---
+++ src/pages/ConnectFour.js
@@ -0,0 +1,8 @@
+import * as React from 'react'
+
+class ConnectFour extends React.component {
+    const [redsTurn, setRedsTurn] = useState(true);
+    const [grid, setGrid] = useState(initialGrid);
+}
+
+export default ConnectFour
