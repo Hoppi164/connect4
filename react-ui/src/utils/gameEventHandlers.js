6ce795f1fc89126fde7398392ab7f92bd6befd15
drew <hoppi164@gmail.com>
2020-08-12 22:10:59 +1000

    tidy up semicolons

--- react-ui/src/utils/gameEventHandlers.js
+++ react-ui/src/utils/gameEventHandlers.js
@@ -1,9 +1,9 @@
 import io from "socket.io-client";
 // Extract current url
-var backendURL = window.location.origin
+var backendURL = window.location.origin;
 // If hosted online - set backend url to heroku app
 if (!backendURL.includes("localhost")) {
-  backendURL = "https://connect-4-backend.herokuapp.com/connect4"
+  backendURL = "https://connect-4-backend.herokuapp.com/connect4";
 }
 console.log("backendURL", backendURL);
 
@@ -26,7 +26,7 @@ const createListeners = (setMessage) => {
     socket.emit("requestRoomEntry", { roomID: "1234" });
   };
   const onJoinedRoom = async (data) => {
-    console.log("Room Joined", data)
+    console.log("Room Joined", data);
     // socket.emit("joinRoom", { roomID: "1234" });
   };
   // const socket = io.connect("http://localhost", {port: 8000, transports: ["websocket"]});
