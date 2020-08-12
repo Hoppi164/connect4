278f357146b9b7a17a2a0c1ba862f987e4f72887
drew <hoppi164@gmail.com>
2020-08-12 21:06:10 +1000

    Updates

---
+++ precache-manifest.6a7778cab1319daeb431c18554c0efb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3c15c295ef5053afe4809f4d876563c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecdc50e9e72eb524b371",
+    "url": "/static/css/main.a73dd17b.chunk.css"
+  },
+  {
+    "revision": "f3bd4d67ab1f31bcde64",
+    "url": "/static/js/2.a28c9141.chunk.js"
+  },
+  {
+    "revision": "ea3b28b1dd7d433e409c33d71ce30398",
+    "url": "/static/js/2.a28c9141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecdc50e9e72eb524b371",
+    "url": "/static/js/main.1b819c85.chunk.js"
+  },
+  {
+    "revision": "041621c0757848d23643",
+    "url": "/static/js/runtime-main.89ce72de.js"
+  }
+]);
