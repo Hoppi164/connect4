5e1d76dbf21d0e7e2b49efb7985b545bf6b1dcbe
drew <hoppi164@gmail.com>
2020-08-13 22:17:51 +1000

    Added Tests

--- react-ui/src/App.test.js
+++ react-ui/src/App.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { screen, render } from "@testing-library/react";
+import App from "./App";
+import ReactDOM from "react-dom";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders without crashing", () => {
+	const div = document.createElement("div");
+	ReactDOM.render(<App />, div);
+});
+
+test("turnTracker exists", () => {
+	const { getByText } = render(<App />);
+	console.log(getByText);
+	const turnTracker = getByText("Red Turn!");
+	expect(turnTracker).toBeInTheDocument();
 });
