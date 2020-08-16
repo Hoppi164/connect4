e1f02ddb1c7b429882d821f791336a0eea6bc5db
drew <hoppi164@gmail.com>
2020-08-16 21:34:34 +1000

    Added code to calculate Win Condition

--- react-ui/src/components/GameBoard.js
+++ react-ui/src/components/GameBoard.js
@@ -19,17 +19,22 @@ const useStyles = makeStyles({
     minWidth: 650,
   },
 });
+let directionsMatrix = {
+  vertical: { south: [1, 0], north: [-1, 0] },
+  horizontal: { east: [0, 1], west: [0, -1] },
+  backward: { southEast: [1, 1], northWest: [-1, -1] },
+  forward: { southWest: [1, -1], northEast: [-1, 1] },
+};
+const numCols = 7;
+const numRows = 6;
+let initialGrid = Array.from({ length: numRows }, () =>
+  Array.from({ length: numCols }, () => 0)
+);
 
 function GameBoard() {
   // Define Const vars
   const classes = useStyles();
-  const numCols = 7;
-  const numRows = 6;
   // Create row x col sized empty array
-  let initialGrid = Array.from({ length: numRows }, () =>
-    Array.from({ length: numCols }, () => 0)
-  );
-
   // Define state Vars
   const [redsTurn, setRedsTurn] = useState(true);
   const [grid, setGrid] = useState(initialGrid);
@@ -43,13 +48,71 @@ function GameBoard() {
       if (!newGrid[rowNum][colnum]) {
         let color = redsTurn ? "Red" : "Yellow"; //determine who's turn it is
         newGrid[rowNum][colnum] = color; // add coin to cell
+        if (playerHasWon(colnum, rowNum, color, newGrid)) {
+          announceWinner(color);
+        }
         setRedsTurn(!redsTurn); // Toggle turn
+        // Update the grid
+        setGrid(newGrid);
         break;
       }
     }
+  };
 
-    // Update the grid
-    setGrid(newGrid);
+  const playerHasWon = (colnum, rowNum, playerColor, newGrid) => {
+    //For each [North/South, East/West, NorthEast/Northwest, SouthEast/Southwest]
+    for (let axis in directionsMatrix) {
+      // We difine this variable here so that "East" and "West" share the same count,
+      // This allows a coin to be dropped in a middle cell
+      let numMatches = 1;
+
+      // For each [North, South]
+      for (let direction in directionsMatrix[axis]) {
+        // Get X/Y co-ordinates of our dropped coin
+        let cellReference = [rowNum, colnum];
+
+        // Add co-ordinates of 1 cell in test direction (eg "North")
+        let testCell = newGrid[cellReference[0]][cellReference[1]];
+
+        // Count how many matching color cells are in that direction
+        while (testCell == playerColor) {
+          try {
+            // Add co-ordinates of 1 cell in test direction (eg "North")
+            cellReference[0] += directionsMatrix[axis][direction][0];
+            cellReference[1] += directionsMatrix[axis][direction][1];
+            testCell = newGrid[cellReference[0]][cellReference[1]];
+
+            // Test if cell is matching color
+            if (testCell == playerColor) {
+              numMatches += 1;
+
+              // If our count reaches 4, the player has won the game
+              if (numMatches >= 4) {
+                return true;
+              }
+            }
+          } catch (error) {
+            // Exceptions are to be expected here.
+            // We wrap this in a try/catch to ignore the array overflow exceptions
+            // console.error(error);
+            break;
+          }
+        }
+        // console.log(`direction: ${direction}, numMatches: ${numMatches}`);
+
+        // If our count reaches 4, the player has won the game
+        if (numMatches >= 4) {
+          return true;
+        }
+      }
+    }
+
+    // If we reach this statement: they have NOT won the game
+    return false;
+  };
+
+  const announceWinner = (winnerColor) => {
+    alert(`WINNER!, ${winnerColor} has won the game!`);
   };
 
   return (
